8315058ae6d38bbe51a7699746caf0ad49eb81ab
Aritra Chakraborty <aritra.chakraborty203@gmail.com>
2025-01-30 19:07:53 +0530

    Update:Update in POST response for expense

--- Backend/Controllers/Expense/expensePOST.js
+++ Backend/Controllers/Expense/expensePOST.js
@@ -5,7 +5,7 @@ export const expensePOST = () => {
     try {
       const expense = new expenseSchemaObj(req.body);
       await expense.save();
-      res.status(201).json({ status: 201, response: expense });
+      res.status(201).json({ status: 201 });
     } catch (error) {
       res.status(400).json({ status: 400, error: error.message });
     }
